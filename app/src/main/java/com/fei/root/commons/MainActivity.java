3aee4eb3c44ff29feeac6eb0d87e79d65f033522
彭飞飞 <feifei.peng@dianrong.com>
2017-08-07 18:38:52 +0800

    commonFromBintray

--- app/src/main/java/com/fei/root/commons/MainActivity.java
+++ app/src/main/java/com/fei/root/commons/MainActivity.java
@@ -3,6 +3,8 @@ package com.fei.root.commons;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.feifei.common.CrashHandler;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
