439d128f7f113d2e2f21b3bbd595501573d6bdc1
RunFeifei <feifei.feixia@163.com>
2017-08-11 22:01:35 +0800

    1

--- app/src/main/java/com/fei/root/commons/ToastActivity.java
+++ app/src/main/java/com/fei/root/commons/ToastActivity.java
@@ -1,11 +1,17 @@
 package com.fei.root.commons;
 
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import com.fei.root.common.AToast;
 import com.fei.root.commons.base.BaseActivity;
 
-import butterknife.OnClick;
+import butterknife.BindView;
 
 /**
  * Created by PengFeifei on 17-8-9.
@@ -13,7 +19,9 @@ import butterknife.OnClick;
 
 public class ToastActivity extends BaseActivity {
 
-    private static int i = 100000000;
+
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
 
     @Override
     protected int getContentLayoutId() {
@@ -22,16 +30,52 @@ public class ToastActivity extends BaseActivity {
 
     @Override
     protected void init(Bundle savedInstanceState) {
-
+        viewpager.setOffscreenPageLimit(3);
+        viewpager.setPageMargin(30);
+        viewpager.setAdapter(new Adapter());
     }
 
-    @OnClick(R.id.btn)
-    public void onBtnShowShortToastSafeClicked() {
-        AToast.get(this).setText("" + i++).show();
-    }
+    private class Adapter extends PagerAdapter {
 
-    @OnClick(R.id.btn1)
-    public void btn1() {
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            RoundImageView imageView = new RoundImageView(ToastActivity.this);
+            if (position == 0) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(ToastActivity.this, R.drawable.banner_01));
+            }
+            if (position == 1) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(ToastActivity.this, R.drawable.banner_02));
+            }
+            if (position == 2) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(ToastActivity.this, R.drawable.banner_03));
+            }
+            imageView.setBackground(ContextCompat.getDrawable(ToastActivity.this, R.drawable.shape_rect_round));
+            container.addView(imageView);
+           /*
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            ViewPager.LayoutParams layoutParams=(ViewPager.LayoutParams)imageView.getLayoutParams();
+
+            imageView.setLayoutParams(layoutParams);*/
+            LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(780,225);
+            container.setLayoutParams(params);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 
 
